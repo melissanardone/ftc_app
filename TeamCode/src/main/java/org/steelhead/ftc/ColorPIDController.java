54c89778db3c41c3c1a89cdea67c574ddde71a9f
Alec Matthews <alec500oo@gmail.com>
2016-10-23 10:19:42 -0700

    Started writing the color sensor PID loop in a separate thread

---
+++ TeamCode/src/main/java/org/steelhead/ftc/ColorPIDController.java
@@ -0,0 +1,40 @@
+package org.steelhead.ftc;
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+/**
+ * Created by Alec Matthews on 10/23/2016.
+ */
+
+public class ColorPIDController implements Runnable {
+    private ColorSensor colorSensor;
+    private int offsetValue;
+    private boolean isOutputAvailable = false;
+    private double output;
+    private boolean isActive = true;
+    private double kp;
+    private double ki;
+    private double kd;
+
+    public void ColorPIDController(ColorSensor colorSensor, int thresholdLow, int thresholdHigh) {
+        this.colorSensor = colorSensor;
+        this.offsetValue = (thresholdLow + thresholdHigh)/2;
+    }
+    public void setPID(double kp, double ki, double kd) {
+        this.kp = kp;
+        this.ki = ki;
+        this.kd = kd;
+    }
+    public boolean isOutputAvailable() {
+        return isOutputAvailable;
+    }
+    public double getOutput() {
+        return output;
+    }
+    @Override
+    public void run() {
+        while (isActive) {
+            //TODO: implement the pid controller for the color sensor READ ARTICLE!!
+        }
+    }
+}
